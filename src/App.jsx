b4da5c178f52aeb91365e22c8825c7ea8979ba1e
ngoduc <ngoduc202@gmail.com>
2024-12-05 20:47:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+      <>
+        {/* Các bạn để các component ở trong đây !! */}
+        <h1 className='text-danger'>Hello Cyber !</h1>
+      </>
+  )
+}
+
+export default App
+
